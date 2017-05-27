9c9b2fea04f33bb4c9bb80e470907aa3392da11d
seedgabo <seedgabo@gmail.com>
2017-05-27 10:12:29 -0500

    register

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -4,95 +4,119 @@ import { Api } from '../../providers/Api';
 import { HomePage } from "../home/home";
 import { Terms } from "../terms/terms";
 @Component({
-    selector: 'page-login',
-    templateUrl: 'login.html'
+  selector: 'page-login',
+  templateUrl: 'login.html'
 })
 export class LoginPage {
-    register = false;
-    password = "";
-    confirm_password = "";
-    nombre = "";
-    username = "";
-    cedula;
-    terms = false;
-    constructor(public navCtrl: NavController, public navParams: NavParams, public api: Api, public alert: AlertController, public loading: LoadingController) { }
+  register = false;
+  send = {
+    password: "",
+    password_confirmation: "",
+    nombre: "",
+    email: "",
+    cedula: "",
+    code: "",
+    cliente_id: 1,
+  }
+  terms = false;
+  constructor(public navCtrl: NavController, public navParams: NavParams, public api: Api, public alert: AlertController, public loading: LoadingController) { }
 
-    ionViewDidLoad() {
+  ionViewDidLoad() {
 
-    }
+  }
 
-    doLogin() {
-        var loading = this.loading.create({ content: "Iniciando Sesión" });
-        loading.present();
-        this.api.doLogin().then((response: any) => {
-            loading.dismiss();
-            if (response.cliente == null) {
-                this.alert.create({ title: "Error", message: "El uso de esta aplicación esta restringido a clientes", buttons: ["Ok"] }).present();
-                return
-            }
-            console.log("last pedido:", response.last_pedido);
+  doLogin() {
+    var loading = this.loading.create({ content: "Iniciando Sesión" });
+    loading.present();
+    this.api.doLogin().then((response: any) => {
+      loading.dismiss();
+      if (response.cliente == null) {
+        this.alert.create({ title: "Error", message: "El uso de esta aplicación esta restringido a clientes", buttons: ["Ok"] }).present();
+        return
+      }
+      console.log("last pedido:", response.last_pedido);
 
 
-            this.api.saveUser(response);
-            this.api.saveData()
-            this.api.user = response;
-            this.navCtrl.setRoot(HomePage);
+      this.api.saveUser(response);
+      this.api.saveData()
+      this.api.user = response;
+      this.navCtrl.setRoot(HomePage);
 
-        }).catch(() => {
-            loading.dismiss();
-            this.alert.create({ title: "Error", message: "Error al iniciar sesión", buttons: ["Ok"] }).present();
-        });
-    }
+    }).catch(() => {
+      loading.dismiss();
+      this.alert.create({ title: "Error", message: "Error al iniciar sesión", buttons: ["Ok"] }).present();
+    });
+  }
 
-    registrarse() {
-        this.register = !this.register;
-    }
+  registrarse() {
+    this.register = !this.register;
+  }
 
-    canRegister() {
-        return (this.password == this.confirm_password && this.password.length > 5 && this.username.length > 5 && this.terms && this.nombre.length > 5 && this.cedula > 1000);
-    }
+  canRegister() {
+    return (this.send.password == this.send.password_confirmation && this.send.password.length > 5 && this.send.email.length > 5 && this.terms && this.send.nombre.length > 5 && this.send.cedula.length > 4);
+  }
 
-    doRegister() {
-        // TODO: register;
-    }
-    verTerms() {
-        this.navCtrl.push(Terms);
-    }
-
-    forgotPassword() {
+  doRegister() {
+    var loading = this.loading.create({ content: "Cargando" });
+    loading.present();
+    this.api.post('register', this.send)
+      .then((data) => {
+        loading.dismiss();
+        console.log(data);
+        this.api.username = this.send.email;
+        this.api.password = this.send.password;
+        this.api.user = data;
+        this.api.saveUser(data);
+        this.api.saveData()
+        this.navCtrl.setRoot(HomePage);
+      }).catch((err) => {
+        loading.dismiss();
+        console.error(err);
         this.alert.create({
-            title: "Olvide mi contraseña",
-            inputs: [
-                {
-                    label: 'email',
-                    type: 'email',
-                    name: 'email',
-                    placeholder: 'Correo'
-                }
-            ],
-            buttons: [
-                {
-                    text: 'Recuperar',
-                    handler: (data) => {
-                        var email = data.email;
-                        this.api.get('forgot-password?email=' + email)
-                            .then((data) => {
-                                this.alert.create({
-                                    title: "correo enviado",
-                                    subTitle: "Revise su bandeja de entrada",
-                                    buttons: ["OK"]
-                                }).present();
-                            })
-                            .catch((err) => {
-                                this.alert.create({
-                                    title: "no se pudo enviar el correo de recuperación",
-                                    buttons: ["OK"]
-                                }).present();
-                            });
-                    }
-                }
-            ]
+          message: err.error + " " + err.error_message,
+          title: "ERROR",
+          buttons: ["OK"],
         }).present();
-    }
+      })
+  }
+  verTerms() {
+    this.navCtrl.push(Terms);
+  }
+
+  forgotPassword() {
+    this.alert.create({
+      title: "Olvide mi contraseña",
+      inputs: [
+        {
+          label: 'email',
+          type: 'email',
+          name: 'email',
+          placeholder: 'Correo'
+        }
+      ],
+      buttons: [
+        {
+          text: 'Recuperar',
+          handler: (data) => {
+            var email = data.email;
+            this.api.get('forgot-password?email=' + email)
+              .then((data) => {
+                this.alert.create({
+                  title: "correo enviado",
+                  subTitle: "Revise su bandeja de entrada",
+                  buttons: ["OK"]
+                }).present();
+              })
+              .catch((err) => {
+                this.alert.create({
+                  title: "no se pudo enviar el correo de recuperación",
+                  buttons: ["OK"]
+                }).present();
+              });
+          }
+        }
+      ]
+    }).present();
+  }
 
 }

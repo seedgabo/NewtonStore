7e974aefaf53de24f613ebb9b6a115e7d40bd914
seedgabo <seedgabo@gmail.com>
2017-06-28 18:38:27 -0500

    some changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, ViewChild } from '@angular/core';
-import { Nav, Platform, AlertController } from 'ionic-angular';
+import { Nav, Platform, AlertController, Events } from 'ionic-angular';
 
 import { CodePush, InstallMode } from '@ionic-native/code-push';
 import { StatusBar } from '@ionic-native/status-bar';
@@ -13,93 +13,96 @@ import { HomePage } from "../pages/home/home";
 import { TutorialPage } from "../pages/tutorial/tutorial";
 import * as moment from 'moment';
 @Component({
-	templateUrl: 'app.html'
+  templateUrl: 'app.html'
 })
 export class MyApp {
-	@ViewChild(Nav) nav: Nav;
-	rootPage: any = HomePage;
-	pages: Array<{ title: string, component: any, icon: string }>;
-	hora = 0;
-	minutos = 0;
-	constructor(public platform: Platform, public api: Api, public alert: AlertController,
-		public codepush: CodePush, public statusbar: StatusBar, public splashscreen: SplashScreen) {
-		this.initializeApp();
-	}
+  @ViewChild(Nav) nav: Nav;
+  rootPage: any = HomePage;
+  pages: Array<{ title: string, component: any, icon: string }>;
+  hora = 0;
+  minutos = 0;
+  constructor(public platform: Platform, public api: Api, public alert: AlertController,
+    public codepush: CodePush, public statusbar: StatusBar, public splashscreen: SplashScreen, public events: Events) {
+    this.initializeApp();
+    events.subscribe('logout', () => {
+      this.logout();
+    })
+  }
 
-	initializeApp() {
-		this.platform.ready().then(() => {
-			this.statusbar.styleDefault();
-			this.splashscreen.hide();
-			if (this.platform.is('android') || this.platform.is('ios')) {
-				const downloadProgress = (progress) => { console.log(`Downloaded ${progress.receivedBytes} of ${progress.totalBytes}`); }
-				this.codepush.sync({ updateDialog: false, installMode: InstallMode.ON_NEXT_RESUME, }, downloadProgress)
-					.subscribe(
-					(syncStatus) => { console.log(syncStatus) },
-					(err) => { console.warn(err) }
-					);
-			}
-		});
-		this.hora = moment().hour();
-		this.minutos = moment().minutes();
-		console.log(this.hora, this.minutos);
-		setInterval(() => {
-			this.hora = moment().hour();
-			this.minutos = moment().minutes();
-			console.log(this.hora, this.minutos);
-		}, 1000 * 60);
-	}
+  initializeApp() {
+    this.platform.ready().then(() => {
+      this.statusbar.styleDefault();
+      this.splashscreen.hide();
+      if (this.platform.is('android') || this.platform.is('ios')) {
+        const downloadProgress = (progress) => { console.log(`Downloaded ${progress.receivedBytes} of ${progress.totalBytes}`); }
+        this.codepush.sync({ updateDialog: false, installMode: InstallMode.ON_NEXT_RESUME, }, downloadProgress)
+          .subscribe(
+          (syncStatus) => { console.log(syncStatus) },
+          (err) => { console.warn(err) }
+          );
+      }
+    });
+    this.hora = moment().hour();
+    this.minutos = moment().minutes();
+    console.log(this.hora, this.minutos);
+    setInterval(() => {
+      this.hora = moment().hour();
+      this.minutos = moment().minutes();
+      console.log(this.hora, this.minutos);
+    }, 1000 * 60);
+  }
 
-	updateUser() {
-		this.api.doLogin().then((response) => {
-			this.api.saveUser(response);
-			this.api.saveData();
-			this.api.user = response;
-		})
-			.catch((err) => {
-				console.error(err);
-			});
-	}
+  updateUser() {
+    this.api.doLogin().then((response) => {
+      this.api.saveUser(response);
+      this.api.saveData();
+      this.api.user = response;
+    })
+      .catch((err) => {
+        console.error(err);
+      });
+  }
 
-	openPage(page) {
-		this.nav.setRoot(page.component);
-	}
+  openPage(page) {
+    this.nav.setRoot(page.component);
+  }
 
-	verTutorial() {
-		this.nav.push(TutorialPage);
-	}
+  verTutorial() {
+    this.nav.push(TutorialPage);
+  }
 
-	verperfil() {
-		this.nav.push(ProfilePage);
-	}
+  verperfil() {
+    this.nav.push(ProfilePage);
+  }
 
-	verMisPedidos() {
-		this.nav.push(PedidosPage);
-	}
+  verMisPedidos() {
+    this.nav.push(PedidosPage);
+  }
 
-	cancelarPedido(tipo) {
-		var pedido = this.api.pedidos_ayer[tipo];
-		this.api.delete('pedidos/' + pedido.id)
-			.then((data) => {
-				this.alert.create({ buttons: ["OK"], title: "El pedido fue cancelado" }).present();
-				this.api.pedidos_ayer[tipo] = null;
-			})
-			.catch((err) => {
-				this.alert.create({ buttons: ["OK"], title: "No se pudo cancelar el pedido" }).present();
-				console.error(err);
-			});
-	}
+  cancelarPedido(tipo) {
+    var pedido = this.api.pedidos_ayer[tipo];
+    this.api.delete('pedidos/' + pedido.id)
+      .then((data) => {
+        this.alert.create({ buttons: ["OK"], title: "El pedido fue cancelado" }).present();
+        this.api.pedidos_ayer[tipo] = null;
+      })
+      .catch((err) => {
+        this.alert.create({ buttons: ["OK"], title: "No se pudo cancelar el pedido" }).present();
+        console.error(err);
+      });
+  }
 
-	logout() {
-		this.api.productos = [];
-		this.api.user = { token: null };
-		this.api.username = "";
-		this.api.password = "";
-		this.api.carrito = [];
-		this.api.storage.clear();
-		this.api.tipo = "";
-		this.api.categorias = [44, 27, 46, 47, 48, 49, 26, 45, 50, 51, 52, 53];
-		this.api.index = 0;
-		this.nav.setRoot(LoginPage);
-	}
+  logout() {
+    this.api.productos = [];
+    this.api.user = { token: null };
+    this.api.username = "";
+    this.api.password = "";
+    this.api.carrito = [];
+    this.api.storage.clear();
+    this.api.tipo = "";
+    this.api.categorias = [44, 27, 46, 47, 48, 49, 26, 45, 50, 51, 52, 53];
+    this.api.index = 0;
+    this.nav.setRoot(LoginPage);
+  }
 
 }

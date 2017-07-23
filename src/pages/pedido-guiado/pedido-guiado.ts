94c0fda48524161ec8bce26c05725ddccc8018e0
seedgabo <seedgabo@gmail.com>
2017-07-23 18:00:43 -0500

    fix categorias

--- src/pages/pedido-guiado/pedido-guiado.ts
+++ src/pages/pedido-guiado/pedido-guiado.ts
@@ -39,41 +39,55 @@ export class PedidoGuiadoPage {
   }
 
   selectproducto(producto) {
-    if (producto == 'none') {
-      this.producto_selected = undefined;
+    if (this.api.index === 0) {
       window.categorias_originales = JSON.parse(JSON.stringify(this.api.categorias));
-      this.api.categorias = this.api.categorias.filter((cat: any) => {
-        return cat.id != 72;
-      })
-    }
-    else {
-      this.producto_selected = producto;
-      if (producto.name && producto.name.indexOf('Ensalada de Fruta') > -1) {
-        window.categorias_originales = JSON.parse(JSON.stringify(this.api.categorias));
-        console.log(this.api.categorias);
-        this.api.categorias = this.api.categorias.filter((cat: any) => {
-          return [63, 72, 50, 45, 61].indexOf(cat.id) > -1;
-        })
-      }
-      else if (producto.description && producto.description.indexOf('Menu Liviano (2)') > -1) {
-        window.categorias_originales = JSON.parse(JSON.stringify(this.api.categorias));
-        this.api.categorias = this.api.categorias.filter((cat: any) => {
-          return [63, 72, 50, 45, 61].indexOf(cat.id) > -1;
-        })
-      }
-      else if (producto.description && producto.description.indexOf('Menu Liviano') > -1) {
-        window.categorias_originales = JSON.parse(JSON.stringify(this.api.categorias));
-        this.api.categorias = this.api.categorias.filter((cat: any) => {
-          return [63, 47, 50, 45, 61].indexOf(cat.id) > -1;
-        })
-      }
-      else {
-        window.categorias_originales = JSON.parse(JSON.stringify(this.api.categorias));
+      if (producto == 'none') {
+        this.producto_selected = undefined;
         this.api.categorias = this.api.categorias.filter((cat: any) => {
           return cat.id != 72;
         })
       }
+      else {
+        this.producto_selected = producto;
+        if (producto.name && producto.name.indexOf('Ensalada de Fruta') > -1) {
+          console.log(this.api.categorias);
+          this.api.categorias = this.api.categorias.filter((cat: any) => {
+            return [63, 50, 45, 61].indexOf(cat.id) > -1;
+          })
+          console.log('condicion ensalada de frutas')
+        }
+        else if (producto.description && producto.description.indexOf('Menu Liviano (2)') > -1) {
+          this.api.categorias = this.api.categorias.filter((cat: any) => {
+            return [63, 72, 50, 45, 61].indexOf(cat.id) > -1;
+          })
+
+          console.log('condicion Menu Liviano (2)', this.api.categorias)
+
+        }
+        else if (producto.description && producto.description.indexOf('Menu Liviano') > -1) {
+          this.api.categorias = this.api.categorias.filter((cat: any) => {
+            return [63, 47, 50, 45, 61].indexOf(cat.id) > -1;
+          })
+          console.log('condicion Menu Liviano')
+
+        }
+        else {
+          this.api.categorias = this.api.categorias.filter((cat: any) => {
+            return cat.id != 72;
+          })
+          console.log('condicion Ninguna')
+        }
+      }
     }
+    else {
+      if (producto == 'none') {
+        this.producto_selected = undefined;
+      }
+      else {
+        this.producto_selected = producto;
+      }
+    }
+
     if (this.cangoNext())
       this.siguiente();
     else
@@ -96,21 +110,16 @@ export class PedidoGuiadoPage {
     if (this.navCtrl.canGoBack()) {
       if (this.api.index > 0)
         this.api.index--;
-      if (this.api.carrito.length > 0) {
-        if (this.api.carrito[this.api.index] &&
-          (
-            (this.api.carrito[this.api.index].name && this.api.carrito[this.api.index].name.indexOf('Ensalada de Fruta') > -1) ||
-            (this.api.carrito[this.api.index].description && this.api.carrito[this.api.index].description.indexOf('Menu Liviano') > -1)
-          )
-        ) {
-          console.log("recuperando categorias");
-          this.api.categorias = window.categorias_originales;
-        }
 
-        console.log(this.api.removeFromCart(this.api.carrito[this.api.carrito.length - 1]));
+      if (this.api.index == 0 && window.categorias_originales) {
+        console.log('recuperando categorias', window.categorias_originales)
+        this.api.categorias = JSON.parse(JSON.stringify(window.categorias_originales));
+        window.categorias_originales = null
       }
-      this.navCtrl.pop();
+
+      console.log(this.api.removeFromCart(this.api.carrito[this.api.carrito.length - 1]));
     }
+    this.navCtrl.pop();
   }
 
   terminar() {
